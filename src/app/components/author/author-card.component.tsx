d3146f85df2efbae5bbc49be83c9ff63034ed941
aveghost <aveghost359@gmail.com>
2025-03-19 14:20:24 +0100

    BGO-4: adding author card component

---
+++ src/app/components/author/author-card.component.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image"
+import {Icon} from '@iconify/react'
+
+interface AuthorCardProps {
+    avatar?: string
+    name: string
+    role?: string
+    description?: string
+    date?: string
+}
+
+const AuthorCard = ({avatar,name,role,description,date}: AuthorCardProps) => {
+    return (
+        <div className="flex gap-4 items-center p-4">
+            {avatar ?
+            <Image
+                src={avatar}
+                alt="author"
+                width={48}
+                height={48}
+                className="rounded-full"
+            />
+            : <Icon icon="qlementine-icons:user-24" width="48" height="48" />}
+            <div className="flex flex-col gap-1">
+                <h3>{name}</h3>
+                <span className="text-sm text-zinc-400">{date}</span>
+                <span className="text-sm text-zinc-400">{role}</span>
+                <p className="text-sm text-zinc-300">{description}</p>
+            </div>
+        </div>
+    )
+}
+
+export default AuthorCard
