36e77a297ef9e0e6a10a748322fe9afa98792a31
aveghost <aveghost359@gmail.com>
2025-03-20 13:58:04 +0100

    BGO-9: Added fixed behavior

--- src/app/components/navigation/navigation.component.tsx
+++ src/app/components/navigation/navigation.component.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import NavLink from "./nav-link.component"
 import Logo from "./logo.component"
+import Link from "next/link"
+import { useState, useEffect } from "react"
 
 const Navigation = () => {
     const links = [
@@ -20,16 +24,23 @@ const Navigation = () => {
             name: "Filmy"
         }
     ]
+
+    const  [scroll, setScroll] = useState(false)
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            setScroll(window.scrollY > 130)
+        })
+    },[])
     return (
-        <nav className="bg-zinc-800 p-3 sticky top-0 z-50 w-full">
+        <nav className={`bg-zinc-800 p-3 z-50 w-full ${scroll ? "sticky-nav" : ""}`}>
             <div className="container mx-auto flex justify-between items-center">
                 <Logo />
                 <ul className="flex items-center gap-8">
                     {links.map(link => <NavLink key={link.name} url={link.url} name={link.name} />)}
                 </ul>
                 <div className="flex gap-4 items-center">
-                    <button className="bg-zinc-600 py-2 px-4 rounded-lg cursor-pointer hover:opacity-70 transition-opacity duration-300">Zaloguj</button>
-                    <button className="border-1 border-zinc-600 cursor-pointer py-2 px-4 rounded-lg hover:bg-zinc-600 hover:opacity-70 transition-all duration-300">Zarejestruj</button>
+                    <Link href={"/login"} className="bg-zinc-600 py-2 px-4 rounded-lg cursor-pointer hover:opacity-70 transition-opacity duration-300">Zaloguj</Link>
+                    <Link href={"/register"} className="border-1 border-zinc-600 cursor-pointer py-2 px-4 rounded-lg hover:bg-zinc-600 hover:opacity-70 transition-all duration-300">Zarejestruj</Link>
                 </div>
             </div>
         </nav>
