65844e9e59bc84c78e40c016491c296cc1cb8012
aveghost <aveghost359@gmail.com>
2025-03-20 16:06:26 +0100

    BGO-9: added mobile menu feature

--- src/app/components/navigation/navigation.component.tsx
+++ src/app/components/navigation/navigation.component.tsx
@@ -3,6 +3,7 @@
 import NavLink from "./nav-link.component"
 import Logo from "./logo.component"
 import Link from "next/link"
+import { Icon } from "@iconify/react/dist/iconify.js"
 import { useState, useEffect } from "react"
 
 const Navigation = () => {
@@ -25,22 +26,40 @@ const Navigation = () => {
         }
     ]
 
-    const  [scroll, setScroll] = useState(false)
+    const [scroll, setScroll] = useState(false)
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen)
+    }
+
     useEffect(() => {
         window.addEventListener('scroll', () => {
             setScroll(window.scrollY > 130)
         })
     },[])
+
     return (
         <nav className={`bg-zinc-800 p-3 z-50 w-full ${scroll ? "sticky-nav" : ""}`}>
             <div className="container mx-auto flex justify-between items-center">
                 <Logo />
-                <ul className="flex items-center gap-8">
+                <ul className="md:flex items-center gap-8 hidden">
                     {links.map(link => <NavLink key={link.name} url={link.url} name={link.name} />)}
                 </ul>
-                <div className="flex gap-4 items-center">
+                <div className="md:flex gap-4 items-center hidden">
                     <Link href={"/login"} className="bg-zinc-600 py-2 px-4 rounded-lg cursor-pointer hover:opacity-70 transition-opacity duration-300">Zaloguj</Link>
                     <Link href={"/register"} className="border-1 border-zinc-600 cursor-pointer py-2 px-4 rounded-lg hover:bg-zinc-600 hover:opacity-70 transition-all duration-300">Zarejestruj</Link>
+                </div>
+                <Icon icon="material-symbols:menu-rounded" width="40" height="40" className="md:hidden cursor-pointer" onClick={toggleMobileMenu} />
+                <div className={`mobile-menu fixed ${isMobileMenuOpen ? "right-0 opacity-100" : "-right-100 opacity-0"} top-0 w-full max-w-[300px] h-full bg-zinc-800 z-50 p-4 shadow-lg shadow-zinc-600 transition-opacity duration-300`}>
+                    <Icon icon="material-symbols:close-rounded" width="40" height="40" className="cursor-pointer" onClick={toggleMobileMenu} />
+                    <ul className="md:hidden items-center gap-8 flex flex-col">
+                        {links.map(link => <NavLink key={link.name} url={link.url} name={link.name} />)}
+                    </ul>
+                    <div className="md:hidden gap-4 mt-10 items-center flex justify-center">
+                        <Link href={"/login"} className="bg-zinc-600 py-2 px-4 rounded-lg cursor-pointer hover:opacity-70 transition-opacity duration-300">Zaloguj</Link>
+                        <Link href={"/register"} className="border-1 border-zinc-600 cursor-pointer py-2 px-4 rounded-lg hover:bg-zinc-600 hover:opacity-70 transition-all duration-300">Zarejestruj</Link>
+                    </div>
                 </div>
             </div>
         </nav>
