30c1e19469492a1eff7d6cae6f71932001d0451e
aveghost <aveghost359@gmail.com>
2025-03-20 13:54:45 +0100

    BGO-7: Adding login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "../components/login-register/login-form.component"
+
+const LoginPage = () => {
+    return (
+        <div className="container mx-auto h-full flex flex-col items-center justify-center grow">
+            <LoginForm />
+        </div>
+    )
+}
+
+export default LoginPage
