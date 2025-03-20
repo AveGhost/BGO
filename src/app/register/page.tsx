b7027765ca7283f0d57984624e3d8668d6e64622
aveghost <aveghost359@gmail.com>
2025-03-20 13:55:27 +0100

    BGO-8: Adding register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "../components/login-register/register-form.component"
+
+const RegisterPage = () => {
+    return (
+        <div className="container mx-auto h-full flex flex-col items-center justify-center grow">
+            <RegisterForm />
+        </div>
+    )
+}
+
+export default RegisterPage
