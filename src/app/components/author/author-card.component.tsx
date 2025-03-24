8ace45e0186130f25a3aa8812e24ae2f7b44a9d8
aveghost <aveghost359@gmail.com>
2025-03-24 15:47:22 +0100

    BGO-17: minor changes

--- src/app/components/author/author-card.component.tsx
+++ src/app/components/author/author-card.component.tsx
@@ -20,7 +20,7 @@ const AuthorCard = ({avatar,name,role,description,date}: AuthorCardProps) => {
                 className="rounded-full"
             />}
             <div className={`flex gap-1 ${avatar ? "flex-col" : "justify-between items-center w-full"}`}>
-                {name &&<h3>{name}</h3>}
+                {name &&<h3 className="text-sm">{name}</h3>}
                 {date &&<span className="text-sm text-zinc-400">{date}</span>}
                 {role && <span className="text-sm text-zinc-400">{role}</span>}
                 {description &&<p className="text-sm text-zinc-300">{description}</p>}
