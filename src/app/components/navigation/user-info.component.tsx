8fa27c353d45cbb5721c0581384e2f7efce67704
aveghost <aveghost359@gmail.com>
2025-04-01 23:43:10 +0200

    BGO-9: added user info component after logged in

---
+++ src/app/components/navigation/user-info.component.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { Icon } from "@iconify/react/dist/iconify.js"
+import AuthorAvatar from "../author/author-avatar.component"
+import AuthorName from "../author/author-name.component"
+import DropDownWrapper from "./dropdown-wrapper.component"
+import { useState } from "react"
+
+interface UserInfoProps {
+    user: {
+        firstName: string
+    }
+}
+
+const UserInfo = ({user}: UserInfoProps) => {
+    const [openDropDown, setOpenDropDown] = useState(false)
+
+    const toggleDropDown = () => {
+        setOpenDropDown(!openDropDown)
+    }
+
+    return (
+        <div className="md:flex gap-4 items-center hidden relative">
+            <AuthorAvatar avatar="/avatar.png" />
+            <AuthorName name={user.firstName} />
+            <Icon icon="material-symbols-light:arrow-drop-down" width="24" height="24" className={`cursor-pointer ${openDropDown ? "rotate-180" : "rotate-0"}`} onClick={toggleDropDown} />
+            {openDropDown && <DropDownWrapper />}
+        </div>
+    )
+}
+
+export default UserInfo
