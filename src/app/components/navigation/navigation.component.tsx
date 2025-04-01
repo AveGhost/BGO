6d1f7e058b98093a64195abee8cd7121f78f3a8c
aveghost <aveghost359@gmail.com>
2025-04-01 23:41:50 +0200

    BGO-9: update navigation component

--- src/app/components/navigation/navigation.component.tsx
+++ src/app/components/navigation/navigation.component.tsx
@@ -1,10 +1,13 @@
 'use client'
 
-import NavLink from "./nav-link.component"
 import Logo from "./logo.component"
-import Link from "next/link"
 import { Icon } from "@iconify/react/dist/iconify.js"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
+import { AuthContext } from "@/app/context/AuthProvider"
+import NavButtons from "./nav-buttons.component"
+import NavList from "./nav-list.component"
+import MobileMenu from "./mobile-menu.component"
+import UserInfo from "./user-info.component"
 
 const Navigation = () => {
     const links = [
@@ -28,6 +31,7 @@ const Navigation = () => {
 
     const [scroll, setScroll] = useState(false)
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+    const user = useContext(AuthContext)?.user
 
     const toggleMobileMenu = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen)
@@ -43,24 +47,13 @@ const Navigation = () => {
         <nav className={`bg-zinc-800 p-3 z-50 w-full ${scroll ? "sticky-nav" : ""}`}>
             <div className="container mx-auto flex justify-between items-center">
                 <Logo />
-                <ul className="md:flex items-center gap-8 hidden">
-                    {links.map(link => <NavLink key={link.name} url={link.url} name={link.name} />)}
-                </ul>
-                <div className="md:flex gap-4 items-center hidden">
-                    <Link href={"/login"} className="bg-zinc-600 py-2 px-4 rounded-lg cursor-pointer hover:opacity-70 transition-opacity duration-300">Zaloguj</Link>
-                    <Link href={"/register"} className="border-1 border-zinc-600 cursor-pointer py-2 px-4 rounded-lg hover:bg-zinc-600 hover:opacity-70 transition-all duration-300">Zarejestruj</Link>
-                </div>
+                <NavList links={links} classes="md:flex hidden" />
+                {!user ?
+                <NavButtons classes="md:flex hidden" />
+                :
+                <UserInfo user={user} />}
                 <Icon icon="material-symbols:menu-rounded" width="40" height="40" className="md:hidden cursor-pointer" onClick={toggleMobileMenu} />
-                <div className={`mobile-menu fixed ${isMobileMenuOpen ? "right-0 opacity-100" : "-right-100 opacity-0"} top-0 w-full max-w-[300px] h-full bg-zinc-800 z-50 p-4 shadow-lg shadow-zinc-600 transition-opacity duration-300`}>
-                    <Icon icon="material-symbols:close-rounded" width="40" height="40" className="cursor-pointer" onClick={toggleMobileMenu} />
-                    <ul className="md:hidden items-center gap-8 flex flex-col">
-                        {links.map(link => <NavLink key={link.name} url={link.url} name={link.name} />)}
-                    </ul>
-                    <div className="md:hidden gap-4 mt-10 items-center flex justify-center">
-                        <Link href={"/login"} className="bg-zinc-600 py-2 px-4 rounded-lg cursor-pointer hover:opacity-70 transition-opacity duration-300">Zaloguj</Link>
-                        <Link href={"/register"} className="border-1 border-zinc-600 cursor-pointer py-2 px-4 rounded-lg hover:bg-zinc-600 hover:opacity-70 transition-all duration-300">Zarejestruj</Link>
-                    </div>
-                </div>
+                <MobileMenu isMobileMenuOpen={isMobileMenuOpen} links={links} toggleMobileMenu={toggleMobileMenu} />
             </div>
         </nav>
     )
