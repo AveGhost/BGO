595a413fe457ae5c1a7762921b9dcd8e531d37d9
aveghost <aveghost359@gmail.com>
2025-04-02 21:02:59 +0200

    BGO-9: Added new element to display based on user role

--- src/app/components/navigation/dropdown-wrapper.component.tsx
+++ src/app/components/navigation/dropdown-wrapper.component.tsx
@@ -1,12 +1,16 @@
 import DropDownElement from "./dropdown-element.component"
 import { Logout } from "@/app/utils/logout"
 
-const DropDownWrapper = () => {
+interface DropDownWrapperProps {
+    role: string
+}
+const DropDownWrapper = ({role}: DropDownWrapperProps) => {
     const logout = Logout()
     return (
         <div className="absolute left-0 w-full min-w-[220px] flex flex-col top-15 bg-zinc-600 p-4 rounded-lg">
             <DropDownElement url="/" name="Dodaj recencje" icon="ic:round-post-add" />
             <DropDownElement url="/" name="Twoje posty" icon="dashicons:admin-post" />
+            {role === 'ADMINISTRATOR' ? <DropDownElement url="/" name="Wszystkie posty" icon="ic:round-all-inbox" /> : null}
             <DropDownElement url="/" name="Edytuj profil" icon="carbon:settings" />
             <DropDownElement url="/" name="Wyloguj" icon="hugeicons:logout-04" onClick={logout} />
         </div>
