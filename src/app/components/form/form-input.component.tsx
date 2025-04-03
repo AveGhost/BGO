6cecd2a8ff7e282b8cdb43bb73bee675110c61d6
aveghost <aveghost359@gmail.com>
2025-04-03 20:28:40 +0200

    Changes icon to be optional insted of required

--- src/app/components/form/form-input.component.tsx
+++ src/app/components/form/form-input.component.tsx
@@ -1,7 +1,7 @@
 import { Icon } from "@iconify/react/dist/iconify.js"
 
 interface FormInputProps {
-    icon: string
+    icon?: string
     type: string
     name: string
     placeholder: string
@@ -11,7 +11,7 @@ interface FormInputProps {
 const FormInput = ({icon, type, placeholder, name, value, event}: FormInputProps) => {
     return (
         <div className="relative w-full">
-            <Icon icon={icon} width="24" height="24" className="absolute top-1/2 right-2 -translate-y-1/2 text-zinc-400" />
+            {icon && <Icon icon={icon} width="24" height="24" className="absolute top-1/2 right-2 -translate-y-1/2 text-zinc-400" />}
             <input type={type} placeholder={placeholder} name={name} value={value} onChange={event} className="w-full py-2 px-4 border-1 border-zinc-600 rounded-lg" />
         </div>
     )
