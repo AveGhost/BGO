c9c2d14676f2c82de7a4f6bc16a653a1093a569b
aveghost <aveghost359@gmail.com>
2025-04-03 20:30:10 +0200

    Added new pages to add game/post(not completed)

--- src/app/dashboard/add-post/page.tsx
+++ src/app/dashboard/add-post/page.tsx
@@ -1,13 +1,63 @@
-import AddGame from "@/app/components/add-post/add-game.component"
+'use client'
+
+import getGames from "@/app/utils/games/GetGame"
+import { useEffect, useState } from "react"
+import FormWrapper from "@/app/components/form/form-wrapper.component"
+import FormInput from "@/app/components/form/form-input.component"
+import FormFile from "@/app/components/form/form-file.component"
+import Image from "next/image"
 const AddPost = () => {
+    const [games, setGames] = useState([])
+    const [previewThumbnail, setPreviewThumbnail] = useState<string | undefined>(undefined)
+
+    const handleImagePreview = (file: File | null) => {
+        if(file) {
+            setPreviewThumbnail(URL.createObjectURL(file))
+        }
+    }
+
+    const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0] || null;
+        handleImagePreview(file);
+    };
+
+    const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+    };
+
+    const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+        const file = event.dataTransfer.files?.[0] || null;
+        handleImagePreview(file);
+    };
+
+    const handleDeletePreview = () => {
+        setPreviewThumbnail(undefined)
+    }
+
+    useEffect(() => {
+        const fetchGames = async () => {
+            try {
+                const response = await getGames()
+                setGames(response.content)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        fetchGames()
+    },[])
+
     return (
-        <div className="container mx-auto h-full flex flex-col items-center justify-center grow">
-            <div className="form w-full max-w-[450px] min-h-[350px] p-4">
-                <div className="flex flex-col gap-4 justify-center items-center h-full">
-                <h1 className="text-2xl text-center mb-6">Dodaj recenzowaną grę</h1>
-                    <AddGame />
+        <div className="container max-w-[1200px] mx-auto py-6">
+            <FormWrapper onSubmit={() => {}} isDataValid={null}>
+                <FormInput icon="material-symbols:title-rounded" type="text" placeholder="Wpisz tytuł recenzji" name="title" value="" event={() => {}} />
+                <FormFile icon="material-symbols:upload-rounded" event={handleFileSelect} previewImage={previewThumbnail} deleteImage={handleDeletePreview} handleDragOver={handleDragOver} handleDrop={handleDrop} />
+                <div className="grid grid-cols-3">
+                    <button>Dodaj pole z obrazkiem</button>
+                    <button>Dodaj Nagłówek</button>
+                    <button>Dodaj treść</button>
                 </div>
-            </div>
+            </FormWrapper>
         </div>
     )
 }
