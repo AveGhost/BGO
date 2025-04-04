a85b9c3a5ba26b7f1396125afc57b0dc3eaed50b
aveghost <aveghost359@gmail.com>
2025-04-04 21:25:16 +0200

    Added types for button and dynamic type choose

--- src/app/components/form/form-button.component.tsx
+++ src/app/components/form/form-button.component.tsx
@@ -1,6 +1,12 @@
-const FormButton = ({text}: { text: string }) => {
+interface FormButtonProps {
+    text: string
+    event?: () => void
+    type?: "submit" | "button"
+}
+
+const FormButton = ({text,event,type}: FormButtonProps) => {
     return (
-        <button type="submit" className="bg-zinc-600 py-2 px-4 rounded-lg cursor-pointer hover:opacity-70 transition-opacity duration-300 w-full">{text}</button>
+        <button type={type ?? "button"} className="bg-zinc-600 py-2 px-4 rounded-lg cursor-pointer hover:opacity-70 transition-opacity duration-300 w-full" onClick={event}>{text}</button>
     )
 }
 
