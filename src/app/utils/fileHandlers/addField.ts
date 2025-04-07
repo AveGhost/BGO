539b00fe65ee58b33e760c059d3c5c38e840ce73
aveghost <aveghost359@gmail.com>
2025-04-07 20:27:54 +0200

    Rename some values

--- src/app/utils/fileHandlers/addField.ts
+++ src/app/utils/fileHandlers/addField.ts
@@ -3,5 +3,5 @@ import { FieldType, Field } from "@/app/mixins/PostField";
 type SetContent = React.Dispatch<React.SetStateAction<Field[]>>;
 
 export const addField = (setContent: SetContent) => (type: FieldType) => {
-    setContent(prevContent => [...prevContent, { id: Date.now(), type, value: "", additional: "" }]);
+    setContent(prevContent => [...prevContent, { id: Date.now(), type, content: "", description: "" }]);
 };
