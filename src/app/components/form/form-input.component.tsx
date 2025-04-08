1359007b28b58c6ef09b91881d0406303d2eff35
aveghost <aveghost359@gmail.com>
2025-04-08 20:01:11 +0200

    Fixed some css issue

--- src/app/components/form/form-input.component.tsx
+++ src/app/components/form/form-input.component.tsx
@@ -12,7 +12,7 @@ const FormInput = ({icon, type, placeholder, name, value, event}: FormInputProps
     return (
         <div className="relative w-full">
             {icon && <Icon icon={icon} width="24" height="24" className="absolute top-1/2 right-2 -translate-y-1/2 text-zinc-400" />}
-            <input type={type} placeholder={placeholder} name={name} value={value} onChange={event} className="w-full py-2 px-4 border-1 border-zinc-600 rounded-lg" />
+            <input type={type} placeholder={placeholder} name={name} value={value} onChange={event} className="w-full py-2 px-4 border-1 border-zinc-600 rounded-lg pr-10" />
         </div>
     )
 }
