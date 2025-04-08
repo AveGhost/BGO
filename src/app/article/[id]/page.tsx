b7df11d3fcd16651bd1b256ace7e49b9a0720f62
aveghost <aveghost359@gmail.com>
2025-04-08 20:04:29 +0200

    Deleted console log

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -26,7 +26,6 @@ const SinglePage = async ({params}: {params: {id: number}}) => {
     const author = news.author
     const content = news.content
     const gameTitle = news.game.title
-    console.log(news)
     return (
         <section className="py-6 container mx-auto">
             <Hero title={title} publishDate={publishDate} author={author} image={thumbnail} />
