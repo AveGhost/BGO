2f51aa23822603a98e784b80e77dac436b3fbde6
aveghost <aveghost359@gmail.com>
2025-04-08 20:03:27 +0200

    Added game list component to show/search added games

---
+++ src/app/components/article/game-list-wrapper.component.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import SearchInput from "../other/search-input.component"
+import { useState } from "react"
+import { GameSearchResults } from "@/app/mixins/GameSearchResults"
+import GameList from "./game-list.component"
+
+const GameListWrapper = () => {
+    const [results, setResults] = useState<GameSearchResults>({content: [], page: {size: 0, totalElements: 0, totalPages: 0, number: 0}})
+    return (
+        <div className="border-1 border-zinc-600 w-full p-4 rounded-lg absolute left-0 max-w-[350px]">
+            <h2 className="text-2xl mb-4">Ostatnio dodane gry</h2>
+            <SearchInput onSearchResults={setResults} />
+            <GameList games={results} />
+        </div>
+    )
+}
+
+export default GameListWrapper
