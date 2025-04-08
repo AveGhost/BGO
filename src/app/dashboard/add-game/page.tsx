e86872de9c80d241db2679da9b91a84d0e049988
aveghost <aveghost359@gmail.com>
2025-04-08 20:04:56 +0200

    Added game list and steps components on the page

--- src/app/dashboard/add-game/page.tsx
+++ src/app/dashboard/add-game/page.tsx
@@ -1,11 +1,18 @@
 import AddGame from "@/app/components/add-post/add-game.component"
+import Steps from "@/app/components/article/steps.component"
+import GameListWrapper from "@/app/components/article/game-list-wrapper.component"
 const AddGamePage = () => {
     return (
-        <div className="container mx-auto h-full flex flex-col items-center justify-center grow">
-            <div className="form w-full max-w-[450px] min-h-[350px] p-4">
-                <div className="flex flex-col gap-4 justify-center items-center h-full">
-                    <h1 className="text-2xl text-center mb-6">Dodaj recenzowaną grę</h1>
-                    <AddGame />
+        <div className="container mx-auto h-full grid place-items-center items-center grow relative">
+            <GameListWrapper />
+            <div className="flex flex-col">
+                <Steps />
+                <span className="text-sm text-rose-600 font-medium mb-4 text-center">* Przed dodaniem gry upewnij się, że owa gra nie jest już dodana.</span>
+                <div className="form w-full min-h-[350px] p-4">
+                    <div className="flex flex-col gap-4 justify-center items-center h-full">
+                        <h1 className="text-2xl text-center mb-6">Dodaj recenzowaną grę</h1>
+                        <AddGame />
+                    </div>
                 </div>
             </div>
         </div>
