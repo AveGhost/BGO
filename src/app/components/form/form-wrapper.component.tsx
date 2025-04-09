c54137e5eaf9960067401a35fedaf727dae050a0
aveghost <aveghost359@gmail.com>
2025-04-09 21:40:03 +0200

    Deleted old version of displaying api messages

--- src/app/components/form/form-wrapper.component.tsx
+++ src/app/components/form/form-wrapper.component.tsx
@@ -1,17 +1,13 @@
-import ErrorMessage from "../login-register/error-message.component";
-
 export interface FormWrapperProps {
     children: React.ReactNode;
     onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-    isDataValid: string | null
 }
-const FormWrapper = ({ children, onSubmit, isDataValid }: FormWrapperProps) => {
+const FormWrapper = ({ children, onSubmit }: FormWrapperProps) => {
     return (
         <>
             <form className="login-form flex flex-col gap-4 w-full" onSubmit={e => onSubmit(e)}>
                 {children}
             </form>
-            {isDataValid && <ErrorMessage message={isDataValid} />}
         </>
     )
 }
