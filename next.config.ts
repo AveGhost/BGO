e3c3fad09edad55b8166f65fc2e8e25609884f7a
aveghost <aveghost359@gmail.com>
2025-04-09 21:33:56 +0200

    Added toast package

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "store-images.s-microsoft.com",
       },
+      {
+        protocol: "https",
+        hostname: "planetagracza.pl",
+      }
     ],
   },
 };
