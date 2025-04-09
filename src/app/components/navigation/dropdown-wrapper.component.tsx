07999ee01680bc9362ffae727918531745b211e0
aveghost <aveghost359@gmail.com>
2025-04-09 21:40:43 +0200

    Added new url to dashboard

--- src/app/components/navigation/dropdown-wrapper.component.tsx
+++ src/app/components/navigation/dropdown-wrapper.component.tsx
@@ -9,7 +9,7 @@ const DropDownWrapper = ({role}: DropDownWrapperProps) => {
     return (
         <div className="absolute left-0 w-full min-w-[220px] flex flex-col top-15 bg-zinc-600 p-4 rounded-lg">
             <DropDownElement url="/dashboard/add-game" name="Dodaj recencje" icon="ic:round-post-add" />
-            <DropDownElement url="/" name="Twoje posty" icon="dashicons:admin-post" />
+            <DropDownElement url="/dashboard/my-posts" name="Twoje posty" icon="dashicons:admin-post" />
             {role === 'ADMINISTRATOR' ? <DropDownElement url="/" name="Wszystkie posty" icon="ic:round-all-inbox" /> : null}
             <DropDownElement url="/" name="Edytuj profil" icon="carbon:settings" />
             <DropDownElement url="/" name="Wyloguj" icon="hugeicons:logout-04" onClick={logout} />
