6e05666aec9c2a8e8225d56db072089dc3c627f5
aveghost <aveghost359@gmail.com>
2025-04-09 21:34:25 +0200

    Minor changes

--- src/app/utils/news/PostNews.ts
+++ src/app/utils/news/PostNews.ts
@@ -6,7 +6,9 @@ export default async function postNews(data: PostFormData) {
     try {
         const response = await api.post("/news",data);
         return response.data;
-    } catch (error) {
+    } catch (error: any) {
         console.error(error);
+        const message = error?.response?.data?.message || "Nieznany błąd"
+        throw new Error(message)
     }
 }
