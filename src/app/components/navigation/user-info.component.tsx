0911a82f3d42e222ffe48206833b29ad605fb1ca
aveghost <aveghost359@gmail.com>
2025-04-10 21:00:48 +0200

    Fixed close dropdown issue

--- src/app/components/navigation/user-info.component.tsx
+++ src/app/components/navigation/user-info.component.tsx
@@ -25,7 +25,7 @@ const UserInfo = ({user}: UserInfoProps) => {
             <AuthorAvatar avatar="/avatar.png" />
             <AuthorName name={user.firstName} />
             <Icon icon="material-symbols-light:arrow-drop-down" width="24" height="24" className={`cursor-pointer ${openDropDown ? "rotate-180" : "rotate-0"}`} onClick={toggleDropDown} />
-            {openDropDown && <DropDownWrapper role={user.userRole} />}
+            {openDropDown && <DropDownWrapper role={user.userRole} toggleDropDown={toggleDropDown} />}
         </div>
     )
 }
