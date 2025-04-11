6cc6afff4ea5ca8e9b9a7c288fbe070401710ef1
aveghost <aveghost359@gmail.com>
2025-04-11 20:39:24 +0200

    Added page params for pagination

--- src/app/utils/news/GetNewsAll.ts
+++ src/app/utils/news/GetNewsAll.ts
@@ -1,8 +1,13 @@
 import api from "@/app/api/api";
+import { pages } from "next/dist/build/templates/app-page";
 
-export default async function getNewsAll() {
+export default async function getNewsAll({page}: {page?: number} = {}) {
     try {
-        const response = await api.get(`/news`, {});
+        const response = await api.get(`/news`, {
+            params:{
+                page: page
+            }
+        });
         return response.data;
     } catch (error) {
         console.error(error);
