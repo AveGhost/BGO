08dd6fd81aebc697b4770878d4bc0c2c5b2efc9e
aveghost <aveghost359@gmail.com>
2025-04-11 20:40:46 +0200

    Minor css changes

--- src/app/components/review/image-review.component.tsx
+++ src/app/components/review/image-review.component.tsx
@@ -9,7 +9,7 @@ const ImageReview = ({image,quote}: ImageReviewProps) => {
     return (
         <div className="grid grid-cols-[180px_1fr] py-6 col-span-2">
             <span className="text-[12px] pr-3 text-zinc-300 place-content-end">{quote}</span>
-            <div className="w-[720px] h-[400px] relative">
+            <div className="w-[840px] h-[450px] relative">
                 <Image
                     src={image}
                     alt="review"
