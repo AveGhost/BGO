38fda04d6cbd3813cd6ac75880566470e4076e21
aveghost <aveghost359@gmail.com>
2025-04-11 20:43:34 +0200

    Added validation that user cannot give more than 10 rating to the game

--- src/app/components/add-post/set-rating.component.tsx
+++ src/app/components/add-post/set-rating.component.tsx
@@ -14,8 +14,16 @@ const SetRating = ({score, setScore, classes}: SetRatingProps) => {
             return "border-red-500 from-red-500/40 from-0% to-100% text-red-500"
         }
     }
+
+    const handleScore = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = Number(e.target.value)
+        if(value <= 10 && value >= 0) {
+            setScore(Number(value))
+        }
+    }
+
     return (
-        <input type="number" placeholder={score.toString()} min={0} max={10} name="score" value={score} onChange={(e) => setScore(Number(e.target.value))} className={`mx-auto text-center rounded-full w-12 h-12 flex justify-center items-center border-1 bg-gradient-form-br bg-gradient-to-tl appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none ${ratingClass(score)} to-zinc-800 font-medium z-10 ${classes}`} />
+        <input type="number" placeholder={score.toString()} min={0} max={10} name="score" value={score} onChange={handleScore} className={`mx-auto text-center rounded-full w-12 h-12 flex justify-center items-center border-1 bg-gradient-form-br bg-gradient-to-tl appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none ${ratingClass(score)} to-zinc-800 font-medium z-10 ${classes}`} />
     )
 }
 
