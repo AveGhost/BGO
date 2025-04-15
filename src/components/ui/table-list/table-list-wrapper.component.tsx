623b136ab78d635799e8d10e85458eb9f3d024ec
aveghost <aveghost359@gmail.com>
2025-04-15 21:14:39 +0200

    Added skeleton loading

--- src/components/ui/table-list/table-list-wrapper.component.tsx
+++ src/components/ui/table-list/table-list-wrapper.component.tsx
@@ -1,17 +1,43 @@
 'use client'
 
 import SearchInput from "../search-input/search-input.component"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { GameSearchResults } from "@/types/GameSearchResults"
 import TableList from "./table-list.component"
+import { Skeleton } from "../skeleton"
+import getGames from "@/utils/games/GetGame"
 
 const TableListWrapper = () => {
     const [results, setResults] = useState<GameSearchResults>({content: [], page: {size: 0, totalElements: 0, totalPages: 0, number: 0}})
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handleSearchResults = (data: GameSearchResults) => {
+        setResults(data);
+    }
+
+    useEffect(() => {
+        const fetchGames = async () => {
+            setIsLoading(true)
+            const data = await getGames()
+            setResults(data)
+            setIsLoading(false)
+        }
+        if(results.content.length === 0) fetchGames()
+    },[results])
+
     return (
         <div className="border-1 border-zinc-600 w-full p-4 rounded-lg absolute left-0 max-w-[350px]">
             <h2 className="text-2xl mb-4">Ostatnio dodane gry</h2>
-            <SearchInput onSearchResults={setResults} />
-            <TableList elements={results} />
+            <SearchInput onSearchResults={handleSearchResults}  />
+            {isLoading ? (
+                <div className="space-y-4">
+                    {[...Array(6)].map((_, i) => (
+                        <Skeleton key={i} className="w-full h-5 rounded-lg mt-6" />
+                    ))}
+                </div>
+            ) : (
+                <TableList elements={results} />
+            )}
         </div>
     )
 }
