05b76da8cfcd808115805028e7fbb9d945bf2e20
aveghost <aveghost359@gmail.com>
2025-04-15 21:15:53 +0200

    Fixed css

--- src/components/ui/article/rating-table-element.component.tsx
+++ src/components/ui/article/rating-table-element.component.tsx
@@ -9,7 +9,7 @@ interface RatingTableElementProps {
 const RatingTableElement = ({icon,text,isPositive}: RatingTableElementProps) => {
     return (
         <li className="flex items-center text-sm gap-2">
-            <Icon icon={icon} width="32" height="32" className={isPositive ? "text-green-700" : "text-red-600"} />
+            <Icon icon={icon} width="32" height="32" className={`${isPositive ? "text-green-700" : "text-red-600"} min-w-8`} />
             <span>{text}</span>
         </li>
     )
