2819450a2f6424efc3beeabc7faff680b9b30852
aveghost <aveghost359@gmail.com>
2025-04-15 21:13:51 +0200

    Remove form select search component and create searchbox component insted

--- src/components/ui/search-input/search-input.component.tsx
+++ src/components/ui/search-input/search-input.component.tsx
@@ -3,21 +3,19 @@
 import FormInput from "../form/form-input.component"
 import { searchGames } from "@/utils/games/SearchGames"
 import { useState, useEffect } from "react"
-import { GameSearchResults } from "@/types/GameSearchResults"
 
 interface SearchInputProps {
-    onSearchResults: (data: GameSearchResults) => void
+    onSearchResults: (data: any) => void
     placeholder?: string
 }
 
-const SearchInput = ({ onSearchResults, placeholder }: SearchInputProps) => {
+const SearchInput = ({ onSearchResults, placeholder}: SearchInputProps) => {
     const [search, setSearch] = useState("")
     const [isSearching, setIsSearching] = useState(false)
     const [debounceResults, setDebounceResults] = useState(search)
 
     const searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value)
-        setIsSearching(true)
     }
 
     useEffect(() => {
@@ -37,8 +35,12 @@ const SearchInput = ({ onSearchResults, placeholder }: SearchInputProps) => {
             onSearchResults(data)
         }
 
-        if(debounceResults.length >= 0) {
+        if(debounceResults.length >= 3) {
+            setIsSearching(true)
             fetchSearch()
+        } else {
+            onSearchResults({content: [], page: {size: 0, totalElements: 0, totalPages: 0, number: 0}})
+            setIsSearching(false)
         }
     },[debounceResults])
 
