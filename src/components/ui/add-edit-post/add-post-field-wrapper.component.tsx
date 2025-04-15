740ae87cfeb06b331207c2d6ea0f7e5b84d26670
aveghost <aveghost359@gmail.com>
2025-04-15 21:18:28 +0200

    Fixed undefinded classes

--- src/components/ui/add-edit-post/add-post-field-wrapper.component.tsx
+++ src/components/ui/add-edit-post/add-post-field-wrapper.component.tsx
@@ -9,8 +9,8 @@ interface PostFieldWrapperProps {
 }
 const PostFieldWrapper = ({deleteField,children,fieldId,iconClass,classes}: PostFieldWrapperProps) => {
     return (
-        <div className={`relative ${classes}`}>
-            <Icon icon="icon-park-outline:close-one" width="24" height="24" className={`text-red-500 min-w-[24px] absolute -top-3 -right-2 z-10 cursor-pointer ${iconClass}`} onClick={() => deleteField(fieldId)}/>
+        <div className={`relative ${classes ? classes : ''}`}>
+            <Icon icon="icon-park-outline:close-one" width="24" height="24" className={`text-red-500 min-w-[24px] absolute -top-3 -right-2 z-10 cursor-pointer ${iconClass ? iconClass : ''}`} onClick={() => deleteField(fieldId)}/>
             {children}
         </div>
     )
