05c8795c4383f07ac497bcc5608e9b5cc87970b9
aveghost <aveghost359@gmail.com>
2025-04-15 21:13:08 +0200

    Refactor form select component

--- src/components/ui/form/form-select/form-select.component.tsx
+++ src/components/ui/form/form-select/form-select.component.tsx
@@ -1,21 +1,48 @@
 import { Icon } from "@iconify/react/dist/iconify.js"
-import FormSelectWrapper from "./form-select-wrapper.component"
+import { useState } from "react"
 interface FormSelectProps {
-    isOpen: boolean
-    onClick: () => void
     icon: string
-    title: string | null
+    title: string
     elements: string[]
-    singleSelect?: (platform: string, id?: number) => void
-    multiSelect?: (category: string) => void
+    isSingle?: boolean
+    onChange: (el: string | string[]) => void
 }
-const FormSelect = ({isOpen,onClick,icon,title,elements,singleSelect,multiSelect}: FormSelectProps) => {
+const FormSelect = ({icon,title,elements,isSingle,onChange}: FormSelectProps) => {
+    const [thisTitle,setThisTitle] = useState<string | string[]>(isSingle ? title : [])
+    const [isOpen,setIsOpen] = useState<boolean>(false)
+
+    const toggleSelect = () => {
+        setIsOpen(!isOpen)
+    }
+
+    const handleSelectChange = (el: string) =>{
+        if(isSingle){
+            setThisTitle(el)
+            onChange(el)
+        } else {
+            setThisTitle(prev => {
+                const currentElement = prev as string[]
+                const selected = currentElement.includes(el) ? currentElement.filter(element => element !== el) : [...currentElement,el]
+                onChange(selected)
+                return selected
+            })
+        }
+    }
+
+    const isSelected = (element: string) => {
+        if (isSingle) return thisTitle === element
+        return (thisTitle as string[]).includes(element)
+    }
+
     return (
-        <div className="w-full relative py-2 px-4 border-1 border-zinc-600 rounded-lg text-zinc-400" onClick={onClick}>
-            <span className={`cursor-pointer w-full block ${title !== '' ? 'text-zinc-300' : ''}`}>{title ? title : 'Wybierz platforme do recenzji:'}
+        <div className="w-full relative py-2 px-4 border-1 border-zinc-600 rounded-lg text-white" onClick={toggleSelect}>
+            <span className={`cursor-pointer w-full block`}>
+                {isSingle ? thisTitle : (thisTitle as string[]).join(", ") || title}
                 <Icon icon={icon} width="24" height="24" className="absolute top-1/2 right-2 -translate-y-1/2 text-zinc-400" />
+                <ul className={`absolute scale-y-0 opacity-0 bg-zinc-600 rounded-lg w-full top-10 right-0 left-0 flex flex-col gap-2 transition-all duration-300 ${isOpen ? "scale-y-100 opacity-100 z-10" : ""}`}>
+                    {elements.map((element, index) => (<li key={index} className={`hover:bg-zinc-500 px-4 py-2 w-full cursor-pointer transition-colors duration-300 text-zinc-400 ${index === 0 ? "rounded-t-lg" : index === elements.length - 1 ? "rounded-b-lg" : ""} ${isSelected(element) ? "bg-zinc-400/20" : ""}`} onClick={() => handleSelectChange(element)}>{element}</li>))}
+                </ul>
             </span>
-            <FormSelectWrapper isOpen={isOpen} elements={elements} singleSelect={singleSelect} multiSelect={multiSelect} />
         </div>
     )
 }
