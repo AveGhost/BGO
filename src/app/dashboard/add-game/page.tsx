9d781bf95995feb16b23fa4e72de9d1fbec3ed37
aveghost <aveghost359@gmail.com>
2025-04-16 20:44:42 +0200

    Remove warrning (not needed anymore)

--- src/app/dashboard/add-game/page.tsx
+++ src/app/dashboard/add-game/page.tsx
@@ -7,7 +7,6 @@ const AddGame = () => {
             <TableListWrapper />
             <div className="flex flex-col">
                 <Steps />
-                <span className="text-sm text-rose-600 font-medium mb-4 text-center">* Przed dodaniem gry upewnij się, że owa gra nie jest już dodana.</span>
                 <div className="form w-full min-h-[350px] p-4">
                     <div className="flex flex-col gap-4 justify-center items-center h-full">
                         <h1 className="text-2xl text-center mb-6">Dodaj recenzowaną grę</h1>
