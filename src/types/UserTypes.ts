0886b7be1bb1cdab589e0372e75b1f6982fa43f9
aveghost <aveghost359@gmail.com>
2025-04-17 16:35:13 +0200

    Updated user types

---
+++ src/types/UserTypes.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id: number
+    email: string
+    bio?: string
+    createdAt: string
+    firstName: string
+    lastName: string
+    userRole: string
+    avatarUrl?: string
+}
