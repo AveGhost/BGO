6247705447654330201d888a175da57cdda854ed
aveghost <aveghost359@gmail.com>
2025-04-17 16:38:04 +0200

    Added more height flexibility

--- src/components/ui/form/form-file/form-file.component.tsx
+++ src/components/ui/form/form-file/form-file.component.tsx
@@ -9,12 +9,12 @@ interface FormFileProps {
     name: string
     update?: (e: React.ChangeEvent<HTMLInputElement>) => void
     children?: React.ReactNode
-    classes?: string
+    height?: string
 }
 
-const FormFile = ({icon, event, handleDragOver, handleDrop, id, name, children, classes}: FormFileProps) => {
+const FormFile = ({icon, event, handleDragOver, handleDrop, id, name, children, height}: FormFileProps) => {
     return (
-        <div className="relative w-full px-4 border-1 border-zinc-600 rounded-lg min-h-[650px] flex flex-col justify-center items-center">
+        <div className={`relative w-full px-4 border-1 border-zinc-600 rounded-lg flex flex-col justify-center items-center ${height ? height : "min-h-[650px]"}`}>
             <div className="border border-dashed border-zinc-400 rounded-lg flex justify-center items-center flex-col" onDragOver={handleDragOver} onDrop={handleDrop}>
                 <input
                     type="file"
