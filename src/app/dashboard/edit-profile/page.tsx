bee4123deea3f2bbcadc68c27851d9f62da383c9
aveghost <aveghost359@gmail.com>
2025-04-17 16:41:28 +0200

    Added edit profile page/component/loading

---
+++ src/app/dashboard/edit-profile/page.tsx
@@ -0,0 +1,11 @@
+import EditProfileWrapper from "@/components/edit-profile/edit-profile-wrapper.component"
+
+const EditProfile = () => {
+    return (
+        <div className="container mx-auto h-full flex flex-col items-center justify-center grow py-6">
+            <EditProfileWrapper />
+        </div>
+    )
+}
+
+export default EditProfile
