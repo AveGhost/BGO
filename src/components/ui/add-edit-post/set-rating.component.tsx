bb33cd5a7b159bc2396f244ead0358f33e0c2a2c
aveghost <aveghost359@gmail.com>
2025-04-18 15:33:34 +0200

    Changed behavior that you cannot type something like 01 in score

--- src/components/ui/add-edit-post/set-rating.component.tsx
+++ src/components/ui/add-edit-post/set-rating.component.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useState } from "react"
+
 interface SetRatingProps {
     score: number
     setScore: React.Dispatch<React.SetStateAction<number>>
@@ -5,6 +9,7 @@ interface SetRatingProps {
 }
 
 const SetRating = ({score, setScore, classes}: SetRatingProps) => {
+    const [localScore, setLocalScore] = useState(score.toString())
     const ratingClass = (rating: number) => {
         if(rating >= 8) {
             return "border-green-700 from-green-700/40 from-0% to-100% text-green-500"
@@ -16,14 +21,16 @@ const SetRating = ({score, setScore, classes}: SetRatingProps) => {
     }
 
     const handleScore = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = Number(e.target.value)
+        const cleaned = e.target.value.replace(/^0+(?=\d)/, '');
+        const value = parseInt(cleaned)
         if(value <= 10 && value >= 0) {
-            setScore(Number(value))
+            setScore((value))
+            setLocalScore((value).toString())
         }
     }
 
     return (
-        <input type="number" placeholder={score.toString()} min={0} max={10} name="score" value={score} onChange={handleScore} className={`mx-auto text-center rounded-full w-12 h-12 flex justify-center items-center border-1 bg-gradient-form-br bg-gradient-to-tl appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none ${ratingClass(score)} to-zinc-800 font-medium z-10 ${classes}`} />
+        <input type="number" placeholder={score.toString()} min={0} max={10} name="score" value={localScore} onChange={handleScore} className={`mx-auto text-center rounded-full w-12 h-12 flex justify-center items-center border-1 bg-gradient-form-br bg-gradient-to-tl appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none ${ratingClass(parseInt(localScore))} to-zinc-800 font-medium z-10 ${classes}`} />
     )
 }
 
